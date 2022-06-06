f944dfd847e12af591ce360445f2af767011c9ce
memreyagci <emre@meyagci.com>
2022-06-06 22:59:20 +0800

    day3 is added

---
+++ src/test/java/com/cydeo/tests/day3_cssSelector_xpath/T5_locators_getText_getAttribute.java
@@ -0,0 +1,35 @@
+package com.cydeo.tests.day3_cssSelector_xpath;
+
+import com.cydeo.utilities.Driver;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class T5_locators_getText_getAttribute {
+    WebDriver driver = Driver.getDriver();
+
+    @BeforeTest
+    public void setup() {
+        driver.get("https://login1.nextbasecrm.com/?forgot_password=yes");
+    }
+
+    @Test
+    public void resetPassword() {
+        WebElement usernameInputBox = driver.findElement(By.name("USER_LOGIN"));
+        usernameInputBox.sendKeys("awrongusername");
+
+        driver.findElement(By.className("login-btn")).click();
+
+        Assert.assertEquals(driver.findElement(By.className("errortext")).getText(), "Login or E-mail not found");
+    }
+
+    @AfterTest
+    public void closeDriver() {
+        driver.close();
+    }
+}
